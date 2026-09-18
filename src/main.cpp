6156a22670c4437e637e7112ac209776817db702
Shubhayang6 <subhayang6@gmail.com>
2026-09-19 01:24:56 +0530

    Add code improvements and error handling.

--- src/main.cpp
+++ src/main.cpp
@@ -4,36 +4,42 @@
 #include <cstdint>
 #include <filesystem>
 
-int parseHeader()
+int parseHeader(const char *modelPath)
 {
-    char buf[8];
-    std::ifstream file("../../models/model.safetensors", std::ios::binary);
+    std::ifstream file(modelPath, std::ios::binary);
 
-    if(!file.is_open())
+    if (!file)
     {
-        std::cerr << "Failed to open model.safetensors\n";
+        std::cerr << "Failed to open model file:"
+                  << modelPath << '\n';
         return 1;
     }
 
-    // Read first 8 bytes
-    file.read(buf, 8); 
+    char buf[8];
+    file.read(buf, 8);
+
+    if (!file)
+    {
+        std::cerr << "Error: Failed to read 8-Byte header from model file!\n";
+        return 1;
+    }
 
     // Copy to uint64_t DataType
     uint64_t n;
-    memcpy(&n, buf,8);
+    memcpy(&n, buf, 8);
 
-    std::cout << "Model file opened successfully!\n";
-
-    std::cout << n;
-
-    file.close();
+    std::cout << "Header size: "<< n << " bytes\n";
 
     return 0;
 }
 
-int main()
+int main(int argc, char *argv[])
 {
+    if(argc < 2)
+    {
+        std::cerr << "Usage: gpt2 <model_path>\n";
+        return 1;
+    }
 
-    parseHeader();
-    return 0;
+    return parseHeader(argv[1]);
 }
